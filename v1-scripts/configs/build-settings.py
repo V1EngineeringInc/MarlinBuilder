eadd73abf05a3b7a89fef00a583e0736a7cfade3
Antti Andreimann <anttix@amazon.com>
2019-12-24 15:06:17 -0600

    Fix PIO environment for Rambo boards

--- v1-scripts/configs/build-settings.py
+++ v1-scripts/configs/build-settings.py
@@ -6,16 +6,16 @@ MACHINES = {
   'MPCNC/Archim1_T8_16T_LCD': { 'env': 'DUE', 'out': 'firmware.bin' },
   'MPCNC/Archim1_T8_16T_LCD_DualEndstop': { 'env': 'DUE', 'out': 'firmware.bin' },
   'MPCNC/Archim2_T8_16T_LCD_16step': { 'env': 'DUE', 'out': 'firmware.bin' },
-  'MPCNC/Rambo_T8_16T_LCD': { 'env': 'megaatmega2560', 'out': 'firmware.hex' },
-  'MPCNC/Rambo_T8_16T_LCD_DualEndstop': { 'env': 'megaatmega2560', 'out': 'firmware.hex' },
+  'MPCNC/Rambo_T8_16T_LCD': { 'env': 'rambo', 'out': 'firmware.hex' },
+  'MPCNC/Rambo_T8_16T_LCD_DualEndstop': { 'env': 'rambo', 'out': 'firmware.hex' },
   'MPCNC/Ramps_T8_16T_LCD_32step': { 'env': 'megaatmega2560', 'out': 'firmware.hex' },
   'MPCNC/Ramps_T8_16T_LCD_32step_DualEndstop': { 'env': 'megaatmega2560', 'out': 'firmware.hex' },
-  'MPCNC/MRambo_T8_16T_LCD': { 'env': 'megaatmega2560', 'out': 'firmware.hex' },
+  'MPCNC/MRambo_T8_16T_LCD': { 'env': 'rambo', 'out': 'firmware.hex' },
   'MP3DP/Ramps_16T_MK': { 'env': 'megaatmega2560', 'out': 'firmware.hex' },
-  'MP3DP/MRambo_16T_aero': { 'env': 'megaatmega2560', 'out': 'firmware.hex' },
+  'MP3DP/MRambo_16T_aero': { 'env': 'rambo', 'out': 'firmware.hex' },
   'MPCNC/SKR1.3_T8_16T_LCD_32step_DualEndstop': { 'env': 'LPC1768', 'out': 'firmware.bin' },
   'ZenXY/Ramps_16T_LCD_32': { 'env': 'megaatmega2560', 'out': 'firmware.hex' },
-  'ZenXY/MiniRambo_16T': { 'env': 'megaatmega2560', 'out': 'firmware.hex' }
+  'ZenXY/MiniRambo_16T': { 'env': 'rambo', 'out': 'firmware.hex' }
 }
 
 import sys
