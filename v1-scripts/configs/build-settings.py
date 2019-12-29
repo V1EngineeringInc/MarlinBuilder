db6541f5f540692f5f596888a5cd12de04f3f58d
Antti Andreimann <anttix@amazon.com>
2019-12-29 00:23:26 -0600

    Add a dual endstop config for SKR Pro 1.1

--- v1-scripts/configs/build-settings.py
+++ v1-scripts/configs/build-settings.py
@@ -14,6 +14,7 @@ MACHINES = {
   'MP3DP/Ramps_16T_MK': { 'env': 'megaatmega2560', 'out': 'firmware.hex' },
   'MP3DP/MRambo_16T_aero': { 'env': 'rambo', 'out': 'firmware.hex' },
   'MPCNC/SKR1.3_T8_16T_LCD_32step_DualEndstop': { 'env': 'LPC1768', 'out': 'firmware.bin' },
+  'MPCNC/SKRPro1.1_T8_16T_LCD_32step_DualEndstop': { 'env': 'BIGTREE_SKR_PRO', 'out': 'firmware.bin' },
   'ZenXY/Ramps_16T_LCD_32': { 'env': 'megaatmega2560', 'out': 'firmware.hex' },
   'ZenXY/MiniRambo_16T': { 'env': 'rambo', 'out': 'firmware.hex' }
 }
