ef749ac8864e3bf2c0781af4f2e7f401614b8d09
Antti Andreimann <anttix@amazon.com>
2020-02-09 20:55:32 -0600

    Added single-axis config for SKR1.3

--- v1-scripts/configs/build-settings.py
+++ v1-scripts/configs/build-settings.py
@@ -13,6 +13,7 @@ MACHINES = {
   'MPCNC/MRambo_T8_16T_LCD': { 'env': 'rambo', 'out': 'firmware.hex' },
   'MP3DP/Ramps_16T_MK': { 'env': 'megaatmega2560', 'out': 'firmware.hex' },
   'MP3DP/MRambo_16T_aero': { 'env': 'rambo', 'out': 'firmware.hex' },
+  'MPCNC/SKR1.3_T8_16T_LCD_32step': { 'env': 'LPC1768', 'out': 'firmware.bin' },
   'MPCNC/SKR1.3_T8_16T_LCD_32step_DualEndstop': { 'env': 'LPC1768', 'out': 'firmware.bin' },
   'MPCNC/SKRPro1.1_T8_16T_LCD_32step_DualEndstop': { 'env': 'BIGTREE_SKR_PRO', 'out': 'firmware.bin' },
   'ZenXY/Ramps_16T_LCD_32': { 'env': 'megaatmega2560', 'out': 'firmware.hex' },
